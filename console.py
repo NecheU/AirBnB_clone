0b787f6dbfe7d0e0d1ef9c6755ce1c68da204cb6
Abu Jehoiada <abujehoiada@gmail.com>
2024-05-18 06:46:41 -0700

    Fixed code

--- console.py
+++ console.py
@@ -2,11 +2,25 @@
 """Module for the console"""
 
 import cmd
-
+from models import *
 
 class HBNBCommand(cmd.Cmd):
     """Console for the app using cmd module"""
     prompt = "(hbnb)"
+
+    def do_create(self, line):
+        """Creates new instance of BaseModel"""
+        if len(line) < 2:
+            print("** class name missing **")
+        else:
+            arg = line.split()
+            print(f"{arg}")
+            if arg == BaseModel:
+                storage = base_model.BaseModel()
+                storage.save()
+                print(storage.id)
+            else:
+                print("** class doesn't exist **")
 
     def do_quit(self, line):
         """Exits the program"""
